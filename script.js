ef5b6787e83a2c3107becbb83e47404413b9f80a
Alayna11 <89653001+Alayna11@users.noreply.github.com>
2021-11-16 13:15:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert("Hello ninja")
+// var x = 5;
+// var y =37;
+
+// var answer= x + y;
+
+// console.log(answer);
+
+var fname = prompt ("please enter your name")
+alert("Hello" + fname + "how are you today")
